ce8f8d54aa36601630fc41445e72ee14e264010a
gagandotcodes <162048383+gagandotcodes@users.noreply.github.com>
2024-04-02 12:34:37 +0530

    Written centralized error handler and api reponse (#3)

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,102 +1,19 @@
+import registerUser from "../services/userServices.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { ApiError } from "../utils/apiError.js"
-import { User } from "../models/userModel.js"
-import { uploadOnCloudinary } from "../utils/cloudinary.js";
-import { ApiResponse } from "../utils/apiResponse.js";
 
-const registerUser = asyncHandler(async (req, res) => {
+const register = async (request, response) => {
 
-    // get request body
-    const body = req.body;
-    console.log('body', body)
+    const body = request.body;
+    const files = request.files;
 
-    // validate password
-    if (body.password === '') {
-        throw new ApiError(400, 'Empty password not allowed!');
+    const result = await registerUser(body, files);
+
+
+    if(!result.success){
+        return response.status(result.statusCode).send(result)
     }
-    if (body.password.length < 8) {
-        throw new ApiError(400, 'Empty password not allowed!');
-    }
-    // validate email
-    await validateEmail(body.email);
-    //check if user exist
-    const user = await User.findOne({ $or: [{ email: body.email }, { username: body.username }] });
-    console.log('user', user)
-
-
-    // // get local file paths for avatar and cover image
-    const avatarLocalPath = req.files?.avatar[0]?.path;
-    let coverImageLocalPath;
-    if (req.files.cover) {
-        coverImageLocalPath = req.files?.cover[0]?.path;
-    }
-
-    // // check if avatar exist
-    if (!avatarLocalPath) {
-        throw new ApiError(400, 'Avatar file is required!');
-    }
-
-    // // upload on cloudinary
-    const avatar = await uploadOnCloudinary(avatarLocalPath, 'Avatar');
-    let cover;
-    if (coverImageLocalPath) {
-        cover = await uploadOnCloudinary(coverImageLocalPath);
-    }
-
-    if (!avatar) {
-        throw new ApiError(400, 'Error in upload avatar on cloudinary');
-    }
-    if (req.files.cover && !cover) {
-        throw new ApiError(400, 'Error in upload cover on cloudinary');
-    }
-
-    const createUser = await User.create(
-        {
-            email: body.email,
-            userName: body.userName.toLowerCase(),
-            fullName: body.fullName,
-            avatar: avatar.url,
-            coverImage: cover?.url || '',
-            password: body.password,
-        }
-    );
-
-    
-    if (createUser) {
-        return res.status(201).json(
-            new ApiResponse(200, createUser, 'User registered sucecssfully!')
-        )
-    }
-
-
-})
-export default registerUser
-
-async function validateEmail(email) {
-    //checks if first letter of email is a alphabet
-    if ((email[0] > 'a' && email[0] < 'z') || (email[0] > 'A' && email[0] < 'Z')) {
-        let count1 = 0;
-        let count2 = 0;
-        let indexOfAt = 0;
-        indexOfAt = email.indexOf('@')
-        // checks that . should be after @
-        if (email.indexOf('.') < email.indexOf('@')) {
-            throw new ApiError(400, 'Email not vaid , enter a valid Email!');
-        }
-     // checks if there are more than one occurance of '@' and '.'
-        for (let char of email) {
-            if ((char > 'a' && char < 'z') || (char > 'A' && char < 'Z') || (char > 0 && char < 10) || char == '@' || char == '.') {
-                if (char == '@') {
-                    count1 = count1 + 1;
-                }
-                if (char == '.') {
-                    count2 = count2 + 1;
-                }
-            }
-        }
-        if (count1 > 1 || count2 > 1 || count1 < 1 || count2 < 1) {
-            throw new ApiError(400, 'Email not vaid , enter a valid Email!');
-        }
-
-    } else { throw new ApiError(400, 'Email not vaid , enter a valid Email!') }
+    return response.status(result.statusCode).send(result)
 }
+
+export {register}
+// export default registerController
